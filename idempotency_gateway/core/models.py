5ffc2cd963d2718631f16a055a9bc711b581deb6
David Berko <daberko364@gmail.com>
2026-06-20 19:06:26 +0000

    created model and run migrations

--- idempotency_gateway/core/models.py
+++ idempotency_gateway/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class IdempotencyRecord(models.Model):
+    idempotency_key = models.CharField(max_length=256, unique=True)
+    request_body_hash = models.CharField(max_length=64)
+    request_body = models.JSONField()
+    response_status = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
